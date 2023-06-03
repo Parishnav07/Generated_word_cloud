2e9109a1bf8ab86f4eefab426a55a023935218df
Parishnav Thokal <120368610+Parishnav07@users.noreply.github.com>
2023-06-03 12:44:52 +0530

    Delete ecom_logic.js

--- ecom_logic.js
+++
@@ -1,9 +0,0 @@
-const close = document.querySelector(".close");
-const open = document.querySelector(".ham");
-const menu = document.querySelector(".menu");
-close.addEventListener("click", () => {
-  menu.style.visibility = "hidden";
-});
-open.addEventListener("click", () => {
-  menu.style.visibility = "visible";
-});
